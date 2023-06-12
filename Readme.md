2699f8bd104d5c9182a59417dd7bfb64ba5120ea
Tu usuario de GitHub aca <Tu email aca>
2023-06-12 11:08:53 -0500

    se agrego el readme al proyecto y se hicieron algunos cambios para el deploy

---
+++ Readme.md
@@ -0,0 +1,38 @@
+# JeanCell - Tienda Virtual de Teléfonos
+
+JeanCell es una tienda virtual de teléfonos inspirada en la tienda de un amigo. Este proyecto está desarrollado utilizando el patrón Modelo-Vista-Controlador (MVC) y cuenta con funcionalidades de administración para gestionar la tienda.
+
+## Características
+
+- **MVC y PHP8:** El proyecto está estructurado siguiendo el patrón MVC, lo que permite una separación clara de responsabilidades y una mayor mantenibilidad. Se utiliza PHP8 para el desarrollo del backend.
+- **MySQL:** La base de datos utilizada es MySQL, donde se almacenan la información de los productos, usuarios y otras entidades relevantes para el funcionamiento de la tienda.
+- **SASS y JS:** Para la parte de diseño, se utiliza el preprocesador SASS para generar estilos CSS de manera más eficiente. Además, se emplea JavaScript para la interactividad y funcionalidades adicionales en el frontend.
+
+## Uso
+
+1. Clona este repositorio en tu máquina local.
+2. Configura la conexión a la base de datos en el archivo `config/database.php`. Asegúrate de proporcionar las credenciales correctas para tu entorno.
+3. Importa la estructura de la base de datos utilizando el archivo SQL proporcionado en la carpeta `database`.
+4. Abre el proyecto en un servidor web compatible con PHP, como Apache o Nginx.
+5. Accede a la aplicación en tu navegador y podrás explorar la tienda virtual de teléfonos.
+
+## Credenciales de Acceso
+
+- **Administrador:**
+  - Correo electrónico: admin@admin.com
+  - Contraseña: 123456
+
+- **Usuario:**
+  - Correo electrónico: correo@correo.com
+  - Contraseña: 123456
+
+## Contribución
+
+Si deseas contribuir a este proyecto, puedes seguir estos pasos:
+
+1. Haz un fork de este repositorio.
+2. Crea una rama con la nueva funcionalidad o corrección que deseas implementar.
+3. Realiza tus cambios y realiza commits descriptivos.
+4. Envía una solicitud de pull con tus cambios.
+
+Agradecemos todas las contribuciones y sugerencias para mejorar este proyecto.
--- views/paginas/mostrarCarrito.php
+++ views/paginas/mostrarCarrito.php
@@ -6,77 +6,66 @@ $total = 0;
 </div>
 <section class="contenedor">
     <h2>Lista del Carrito</h2>
-    <?php if(!empty($_SESSION['carrito'])){ ?>
-    <div class="contedor-carrito">
-    <table class="table-carrito">
-        <thead>
-            <tr>
-                <th>nombre</th>
-                <th>precio</th>
-                <th>cantidad</th>
-                <th>total</th>
-                <th>--</th>
-            </tr>   
-            <?php foreach($_SESSION['carrito'] as $key => $producto):  ?>
+    <?php if (!empty($_SESSION['carrito'])) { ?>
+        <div class="contedor-carrito">
+            <table class="table-carrito">
+                <thead>
+                    <tr>
+                        <th>nombre</th>
+                        <th>precio</th>
+                        <th>cantidad</th>
+                        <th>total</th>
+                        <th>--</th>
+                    </tr>
+                    <?php foreach ($_SESSION['carrito'] as $key => $producto) :  ?>
 
-                
-            <tr class="incrementar">
-                <td><?php echo $producto['nombre'];  ?></td>
-                <td><?php echo $producto['precio'];  ?></td>
-                <td><?php echo $producto['cantidad']; ?></td>
-                <td><?php echo number_format($producto['precio']*$producto['cantidad'],2);  ?></td>
-                <td>
-                    <form class="td-button"action="" method="POST">
-                        <input type="hidden" name="id" value="<?php echo openssl_encrypt($producto['id'], COD, KEY) ?>">
 
-                        <button type="submit"
-                        name="carrito"
-                        class="boton-rojo"
-                        value="eliminar"
-                        >Eliminar</button>
-                    </form>
-                </td>
-            </tr>
-            <?php $total = $total + ($producto['precio']*$producto['cantidad']); ?>
-            <?php endforeach;  ?>
-            <tr>
-                <td colspan="3">total</td>
-                <td><?php echo number_format($total, 2)  ?></td>
-            </tr>
+                        <tr class="incrementar">
+                            <td><?php echo $producto['nombre'];  ?></td>
+                            <td><?php echo $producto['precio'];  ?></td>
+                            <td><?php echo $producto['cantidad']; ?></td>
+                            <td><?php echo number_format($producto['precio'] * $producto['cantidad'], 2);  ?></td>
+                            <td>
+                                <form class="td-button" action="" method="POST">
+                                    <input type="hidden" name="id" value="<?php echo openssl_encrypt($producto['id'], COD, KEY) ?>">
 
-            <tr>
-                <td colspan="5">
-                    <form action="/pagar" method="POST" class="formulario">
-                    <div class="alerta exito">
-                        <div>
-                        <label for="email">Correo de contacto</label>
-                        <input type="email" name="email" id="email" placeholder="Por favor escribe tu correo" required>
-                        </div>
-                        <small class="">
-                            los productos se enviaran a este correo
-                        </small>
-                        <button
-                        class="boton"
-                        type="submit"
-                        value="agregar"
-                        name="carrito">
-                        proceder a pagar >>
-                        </button>
+                                    <button type="submit" name="carrito" class="boton-rojo" value="eliminar">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                        <?php $total = $total + ($producto['precio'] * $producto['cantidad']); ?>
+                    <?php endforeach;  ?>
+                    <tr>
+                        <td colspan="3">total</td>
+                        <td><?php echo number_format($total, 2)  ?></td>
+                    </tr>
 
-                    </div>
-                    </form>
-                   
-                </td>
-            </tr>
-        </thead>
-    </table>
-    </div>
-    <?php }else{?>
+                </thead>
+            </table>
+        </div>
+    <?php } else { ?>
         <p class="alerta exito">no hay ningun producto en el carrito</p>
     <?php }  ?>
+
+    <div class="contenedor-pagar">
+        <form action="/pagar" method="POST" class="formulario">
+            <div class="alerta exito">
+                <div>
+                    <label for="email">Correo de contacto</label>
+                    <input type="email" name="email" id="email" placeholder="Por favor escribe tu correo" required>
+                </div>
+                <small class="">
+                    los productos se enviaran a este correo
+                </small>
+                <button class="boton" type="submit" value="agregar" name="carrito">
+                    proceder a pagar >>
+                </button>
+
+            </div>
+        </form>
+    </div>
 </section>
 <div class="footer-fixed">
-<?php include __DIR__ . '/../templates/footer.php';
-?>
+    <?php include __DIR__ . '/../templates/footer.php';
+    ?>
 </div>
-
